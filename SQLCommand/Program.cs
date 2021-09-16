4dd58f825bc919838ed49ea712723bf07c8ed04f
mmraihan <mm_raihan@live.com>
2021-09-17 00:41:33 +0600

    Add project files.

---
+++ SQLCommand/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace SQLCommand
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program.connection();
+            Console.ReadKey();
+        }
+
+       static void connection()
+        {
+            
+    
+            string cs = ConfigurationManager.ConnectionStrings["dbcs"].ConnectionString;
+            SqlConnection con = null;
+
+            
+
+            try
+            {
+                using (con=new SqlConnection(cs) )
+                {
+                    con.Open();
+
+                    if (con.State == ConnectionState.Open)
+                    {
+                        Console.WriteLine("Connected");
+                    }
+                }
+               
+            }
+            catch (SqlException ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+
+            finally
+            {
+                con.Close();
+            }
+
+           
+        }
+    }
+}
